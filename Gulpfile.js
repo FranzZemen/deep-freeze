5f1bd7303c572890a97c15eda361dfad65fa627a
Franz Zemen <franzzemen@gmail.com>
2022-10-29 07:55:34 -0400

    Removed unused npm dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,31 +1,10 @@
 import {cwd} from 'node:process';
 import * as gulpBase from '@franzzemen/gulp-base';
-import { createRequire } from "module";
-import {join, dirname} from 'node:path';
-import {npmu as npmuFunc} from '@franzzemen/npmu';
-import {fileURLToPath} from 'url';
+import { createRequire } from "node:module";
 
 const requireModule = createRequire(import.meta.url);
 gulpBase.init(requireModule('./package.json'), cwd() + '/tsconfig.src.json', cwd() + '/tsconfig.test.json', 100);
 gulpBase.setMainBranch('main');
-
-
-export const npmu  = (cb) => {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-
-  npmuFunc([
-    {
-      path: join(__dirname, '../gulp-base'), packageName: '@franzzemen/gulp-base',
-    }, {
-      path: join(__dirname, '../npmu'), packageName: '@franzzemen/npmu',
-    }])
-    .then(() => {
-      console.log('cb...');
-      cb();
-    })
-}
-
-
 
 export const test = gulpBase.test;
 
